a03a95197650593ba52c4cfc09ed3fab716c2132
Umut hakan demir <107408444+umuthakan0@users.noreply.github.com>
2022-07-22 22:27:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ print("lutfen saati ayarlayın")
 second=int(input("saniyeyi girin: "))
 minute=int(input("dakikayı girin: "))
 hour=int(input("saati girin: "))
+day=int(input("günü giriniz: "))
+month=int(input("ayı giriniz: "))
+yıl=int(input("yılı giriniz: "))
 
 while True:
     second+=1
@@ -15,5 +18,21 @@ while True:
         hour+=1
         minute=0
         second=0
-    print("saat:",hour,".",minute,".",second)
-
+    elif hour==24:
+        day+=1
+        minute = 0
+        second = 0
+        hour=0
+    elif day==30:
+        month+=1
+        minute = 0
+        second = 0
+        hour = 0
+        day==0
+    elif month==12:
+        yıl+=1
+        minute = 0
+        second = 0
+        hour = 0
+        month=0
+    print("tarih: ",yıl,",",month,",",day,",",hour,".",minute,".",second)
